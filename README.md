a9be243ec3cee2104b711251dd46fc60f6a108a1
Hesam Amoualian <h.amoualian@wholesoftmarket.com>
2023-03-24 14:22:44 +0100

    add new parts

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Cytoscape_Neo4j
+This repository provides an example of building a directed graph in Neo4j and visualizing the results using Cytoscape. The project shows how to create a free sample of the AuraDB Neo4j cloud database and import data from a JSON file. The code and instructions provided can be used as a starting point for building more complex graph databases and visualizations.
+
+To use this project, follow these steps:
+
+Create a free sample of the AuraDB Neo4j cloud database by following the instructions in the "Create a free sample of the AuraDB database" section of the README file.
+Download the JSON file from the repository and use the provided Notebook to import the data.
+Run the code to visualize the directed graph using Cytoscape.
+For more information about Neo4j and graph databases, visit the Neo4j website at https://neo4j.com/.
+
+For more information about Cytoscape and graph visualization, visit the Cytoscape website at https://cytoscape.org/.
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import expandCollapse from "cytoscape-expand-collapse";
 import popper from 'cytoscape-popper';
 import tippy from "tippy.js";
 import cxtmenu from 'cytoscape-cxtmenu';
+import neo4j from "neo4j-driver";
 var jquery = require("jquery");
 
 cytoscape.use(cxtmenu);
@@ -13,9 +14,9 @@ cytoscape.use(popper);
 
 
 // first connect to the Neo4j database
-const url = "The url of your database";
+const url = "The URL of your database";
 const user = "neo4j";
-const password = "Your_password";
+const password = "Your password";
 var driver = neo4j.driver(
   url,
   neo4j.auth.basic(user, password)
@@ -35,24 +36,20 @@ session
     result.records.forEach(function (record) {
       var node_1 = record._fields[0].properties;
       var node_2 = record._fields[2].properties;
-      var link = record._fields[1].properties;
       var new_node_1 = {
         data: {
-          id: node_1.firstName + " " + node_1.lastName,
-          color: node_1.color,
+          id: node_1.id,
         },
       };
       var new_node_2 = {
         data: {
-          id: node_2.firstName + " " + node_2.lastName,
-          color: node_2.color,
+          id: node_2.id,
         },
       };
       var new_link = {
         data: {
-          source: node_1.firstName + " " + node_1.lastName,
-          target: node_2.firstName + " " + node_2.lastName,
-          color_link: link.color_link,
+          source: node_1.id,
+          target: node_2.id,
         },
       };
       new_nodes.push(new_node_1);
@@ -63,119 +60,117 @@ session
   .then(function () {
     session.close();
     driver.close();
-  });
+
+
+
+    // define general dagre layout
+    var layout = {
+      name: "dagre",
+      rankDir: "LR",
+      align: 'LR',
+      animate: true,
+      nodeDimensionsIncludeLabels: true,
+      padding: 35,
+      rankSep: 250,
+      nodeSep: 10,
+
+    };
+
+    // define expandCollapse layout
+    var cy = (window.cy = cytoscape({
+      container: document.getElementById("cy"),
+
+      boxSelectionEnabled: true,
+      autounselectify: true,
+
+      layout: layout,
+
+      style: [
+        {
+          selector: "node",
+          style: {
+            "content": "data(id)",
+            "text-valign": "center",
+            "text-halign": "center",
+            "height": "110px",
+            "width": "110px",
+            "background-color": "blue",
+            "color": "#FFFFFF",
+            "font-family": "Georgia, serif",
+            "font-size": "14px",
+          }
+        },
+
+        {
+          selector: "edge",
+          style: {
+            "curve-style": "bezier",
+            "control-point-weight": 0.5,
+            "edge-distances": "node-position",
+            width: 2,
+            "target-arrow-shape": "triangle",
+            "line-color": "data(color_link)",
+            "target-arrow-color": "data(color_link)",
+          }
+        }
+      ],
+
+      elements: {
+        nodes: new_nodes,
+        edges: new_links
+      },
+    }));
+
+
+    // add event listeners for zooming in and out
+    document.getElementById("zoom-in").addEventListener("click", function () {
+      cy.zoom({
+        level: cy.zoom() * 1.5,
+        renderedPosition: { x: cy.width() / 2, y: cy.height() / 2 }
+      });
+    });
+
+    document.getElementById("zoom-out").addEventListener("click", function () {
+      cy.zoom({
+        level: cy.zoom() * 0.5,
+        renderedPosition: { x: cy.width() / 2, y: cy.height() / 2 }
+      });
+    });
 
 
 
 
-// define general dagre layout
-var layout = {
-  name: "dagre",
-  rankDir: "LR",
-  align: 'LR',
-  animate: true,
-  nodeDimensionsIncludeLabels: true,
-  padding: 35,
-  rankSep: 350,
-  nodeSep: 10,
+    // add click event listener for nodes
+    cy.on("tap", "node", function () {
+      let node = this;
+      let children = node.outgoers("node");
 
-};
+      if (children.length > 0) {
+        for (let i = 0; i < children.length; i++) {
+          // If the node has children
+          if (children[i].visible()) {
+            // If children are visible, hide them
+            children[i].hide();
+            let grand_children = children[i].outgoers("node");
+            grand_children.hide();
+          } else {
+            // If children are hidden, show them
+            children[i].show();
+            let grand_children = children[i].outgoers("node");
+            grand_children.show();
+          }
 
-// define expandCollapse layout
-var cy = (window.cy = cytoscape({
-  container: document.getElementById("cy"),
-
-  boxSelectionEnabled: true,
-  autounselectify: true,
-
-  layout: layout,
-
-  style: [
-    {
-      selector: "node",
-      style: {
-        "content": "data(id)",
-        "text-valign": "center",
-        "text-halign": "center",
-        "height": "110px",
-        "width": "110px",
-        "background-color": "blue",
-        "color": "#FFFFFF",
-        "font-family": "Georgia, serif",
-        "font-size": "14px",
+        }
+        // Run layout again to update positions
+        cy.layout(layout).run();
       }
-    },
+    });
 
-    {
-      selector: "edge",
-      style: {
-        "curve-style": "bezier",
-        "control-point-weight": 0.5,
-        "edge-distances": "node-position",
-        width: 2,
-        "target-arrow-shape": "triangle",
-        "line-color": "data(color_link)",
-        "target-arrow-color": "data(color_link)",
-      }
-    }
-  ],
-
-  elements: {
-    nodes: new_nodes,
-    edges: new_links
-  },
-}));
-
-
-// add event listeners for zooming in and out
-document.getElementById("zoom-in").addEventListener("click", function () {
-  cy.zoom({
-    level: cy.zoom() * 1.5,
-    renderedPosition: { x: cy.width() / 2, y: cy.height() / 2 }
-  });
-});
-
-document.getElementById("zoom-out").addEventListener("click", function () {
-  cy.zoom({
-    level: cy.zoom() * 0.5,
-    renderedPosition: { x: cy.width() / 2, y: cy.height() / 2 }
-  });
-});
-
-
-
-
-// add click event listener for nodes
-cy.on("tap", "node", function () {
-  let node = this;
-  let children = node.outgoers("node");
-
-  if (children.length > 0) {
-    for (let i = 0; i < children.length; i++) {
-      // If the node has children
-      if (children[i].visible()) {
-        // If children are visible, hide them
-        children[i].hide();
-        let grand_children = children[i].outgoers("node");
-        grand_children.hide();
-      } else {
-        // If children are hidden, show them
-        children[i].show();
-        let grand_children = children[i].outgoers("node");
-        grand_children.show();
-      }
-
-    }
-    // Run layout again to update positions
-    cy.layout(layout).run();
-  }
-});
-
-// create the popper for the node
-function create_popper_content(ele) {
-  var d = ele.data();
-  if (d.lastName == "YOU") {
-    return `
+    // create the popper for the node
+    function create_popper_content(ele) {
+      var d = ele.data();
+      if (d.lastName == "YOU") {
+        return `
     <div class="en-card" >
           <div class="en-card-header" style= 'border: 1px solid #D3D6DA !important; border-radius: 5px;
           padding: 6px;padding-right: 10px;opacity:1;
@@ -186,8 +181,8 @@ function create_popper_content(ele) {
               
    </div>
     `
-  } else {
-    return `
+      } else {
+        return `
     <div class="en-card" style='padding: 6px; margin-right: 8px; border: 1px solid #D3D6DA; background: white; border-radius: 5px;'>
           <div class="en-card-header">
               <span style="color:#48558A;font-size:14px;">
@@ -196,70 +191,73 @@ function create_popper_content(ele) {
               
    </div>
     `
-  }
-}
-
-
-// use tippy for hovering over the node
-function makePopper(ele) {
-  let ref = ele.popperRef();
-
-  ele.tippy = tippy(document.createElement('div'), {
-    getReferenceClientRect: ref.getBoundingClientRect,
-    content: () => {
-      let content = document.createElement('div');
-      content.innerHTML = create_popper_content(ele);
-      return content;
-    },
-    onHidden(instance) {
-      instance.destroy();
-    },
-    interactive: true,
-    appendTo: document.body // or append dummyDomEle to document.body
-  });
-  ele.tippy.show();
-  ele.on('mouseout', function (e) {
-    setTimeout(function () {
-      ele.tippy.hide();
+      }
     }
-      , 100);
-  }
-  );
-}
-
-cy.on('mouseover', 'node', function (e) {
-  makePopper(e.target);
-});
 
 
+    // use tippy for hovering over the node
+    function makePopper(ele) {
+      let ref = ele.popperRef();
 
-// use changeDir button to change the direction of the graph
-var currentDir = null;
-var changeDir = document.getElementById("changeDir");
-changeDir.addEventListener("click", function () {
-  // check if we already have a direction
-  if (currentDir === null) {
-    currentDir = cy.options().layout.rankDir;
-  }
-  // change the direction
-  var newDir = currentDir === "LR" ? "UD" : "LR";
-  // update the layout
-  layout = {
-    name: "dagre",
-    rankDir: newDir,
-    align: 'LR',
-    animate: true,
-    nodeDimensionsIncludeLabels: true,
-    padding: 10,
-    rankSep: 500,
-  };
+      ele.tippy = tippy(document.createElement('div'), {
+        getReferenceClientRect: ref.getBoundingClientRect,
+        content: () => {
+          let content = document.createElement('div');
+          content.innerHTML = create_popper_content(ele);
+          return content;
+        },
+        onHidden(instance) {
+          instance.destroy();
+        },
+        interactive: true,
+        appendTo: document.body // or append dummyDomEle to document.body
+      });
+      ele.tippy.show();
+      ele.on('mouseout', function (e) {
+        setTimeout(function () {
+          ele.tippy.hide();
+        }
+          , 100);
+      }
+      );
+    }
 
-  cy.layout(layout).run();
+    cy.on('mouseover', 'node', function (e) {
+      makePopper(e.target);
+    });
 
-  // update the button text
-  changeDir.innerHTML = "Change direction to " + currentDir;
 
-  // keep the newDir for next time
-  currentDir = newDir;
 
-});
+    // use changeDir button to change the direction of the graph
+    var currentDir = null;
+    var changeDir = document.getElementById("changeDir");
+    changeDir.addEventListener("click", function () {
+      // check if we already have a direction
+      if (currentDir === null) {
+        currentDir = cy.options().layout.rankDir;
+      }
+      // change the direction
+      var newDir = currentDir === "LR" ? "UD" : "LR";
+      // update the layout
+      layout = {
+        name: "dagre",
+        rankDir: newDir,
+        align: 'LR',
+        animate: true,
+        nodeDimensionsIncludeLabels: true,
+        padding: 35,
+        rankSep: 250,
+        nodeSep: 10,
+      };
+
+      cy.layout(layout).run();
+
+      // update the button text
+      changeDir.innerHTML = "Change direction to " + currentDir;
+
+      // keep the newDir for next time
+      currentDir = newDir;
+
+    });
+
+  });
